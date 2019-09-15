639773fa0be60c0b3068fa109e5a8b31b75dec68
Spacedestructor <Spacedestructor@users.noreply.github.com>
2019-09-15 14:28:06 +0200

    Version split
    updated files in the project folder and updated build info to the current experimental release, since non released experimentals will now be aviable in the experimental branch.

--- main.js
+++ main.js
@@ -2,179 +2,210 @@ var Version = "TBA";
 var Twitter = "https://www.twitter.com/spacedestructor";
 var Mega = "https://mega.nz/#F!StxWBKLI!gl8h8d8vzIEyu1qHU1zrXQ";
 var Space = {
+    Name:'Space',
     Total:1000,
     Click:1000,
     Second:0,
     Max:1000
 },
 Grain = {
+    Name:'Grain',
     Total:0,
     Click:1,
     Second:0,
     Max:1000
 },
 Wood = {
+    Name:'wood',
     Total:0,
     Click:1,
     Second:0,
     Max:1000
 },
 Stone = {
+    Name:'Stone',
     Total:0,
     Click:1,
     Second:0,
     Max:1000
 },
 Coal = {
+    Name:'Coal',
     Total:0,
     Click:1,
     Second:0,
     Max:1000
 },
 Iron = {
+    Name:'Iron',
     Total:0,
     Click:1,
     Second:0,
     Max:1000
 },
 Gold = {
+    Name:'Gold',
     Total:0,
     Click:1,
     Second:0,
     Max:100
 },
 Knowledge = {
+    Name:'Knowledge',
     Total:0,
     Click:1,
     Second:0,
     Max:1000
 },
 Synthe = {
+    Name:'Synthe',
     Total:1,
     Click:1,
     Second:0,
     Max:1000,
 },
 Axe = {
+    Name:'Axe',
     Total:1,
     Click:1,
     Second:0,
     Max:1000,
 },
 Pickaxe = {
+    Name:'Pickaxe',
     Total:1,
     Click:1,
     Second:0,
     Max:1000
 },
 Hammer = {
+    Name:'Hammer',
     Total:1,
     Click:1,
     Second:0,
     Max:1000
 },
 Sword = {
+    Name:'Sword',
     Total:0,
     Click:1,
     Second:0,
     Max:1000
 },
 Book = {
+    Name:'Book',
     Total:0,
     Click:1,
     Second:0,
     Max:1000
 },
 Population = {
+    Name:'Population',
     Total:0,
     Click:0,
     Second:0,
     Max:1000
 },
 House = {
+    Name:'House',
     Total:0,
     Click:1,
     Second:0
 },
 Farm = {
+    Name:'Farm',
     Total:0,
     Click:1,
     Second:0
 },
 Lumbercamp = {
+    Name:'Lumbercamp',
     Total:0,
     Click:1,
     Second:0
 },
 Mine = {
+    Name:'Mine',
     Total:0,
     Click:1,
     Second:0
 },
 Castle = {
+    Name:'Castle',
     Total:0,
     Click:1,
     Second:0
 },
 Barracks = {
+    Name:'Barracks',
     Total:0,
     Click:1,
     Second:0,
 },
 Library = {
+    Name:'Library',
     Total:0,
     Click:1,
     Second:0
 },
 Farmer = {
+    Name:'Farmer',
     Total:0,
     Click:1,
     Second:0,
     Max:0
 },
 Woodcutter = {
+    Name:'Woodcutter',
     Total:0,
     Click:1,
     Second:0,
     Max:0
 },
 Miner = {
+    Name:'Miner',
     Total:0,
     Click:1,
     Second:0,
     Max:0
 },
 Blacksmith = {
+    Name:'Blacksmith',
     Total:0,
     Click:1,
     Second:0,
     Max:0
 },
 Wiseman = {
+    Name:'Wiseman',
     Total:0,
     Click:1,
     Second:0,
     Max:0
 },
 Scout = {
+    Name:'Scout',
     Total:0,
     Click:1,
     Second:0,
     Max:0
 },
 Knight = {
+    Name:'Knight',
     Total:0,
     Click:1,
     Second:0,
     Max:0
 },
 General = {
+    Name:'General',
     Total:0,
     Click:1,
     Second:0,
     Max:0
 },
 EmpireStorage = {
+    Name:'Empire Storage',
     Total:1000,
     Click:1000,
     Second:0
@@ -184,22 +215,34 @@ SessionCounter,
 CategoryResurces = true;
 
 if (typeof(window.localStorage) !== "undefined") {
-    browserdoeslocalstorage == 'Great, your Browser does support games save function!',
+    browserdoeslocalstorage == "Great, your Browser does support games save function!",
     document.getElementById("savefunction").innerHTML = browserdoeslocalstorage;
 } else {
-    browserdoeslocalstorage == 'Oh no, your Browser does not support games save function!',
+    browserdoeslocalstorage == "Oh no, your Browser does not support games save function!",
     document.getElementById("savefunction").innerHTML = browserdoeslocalstorage;
 }
-CountSession();
+CountSession(); {
     counter();
+}
+
+//function bake_cookie(name, value) {
+//	var exdate=new Date();
+//	exdate.setDate(exdate.getDate() + 30);
+//	var cookie = [name, '=', JSON.stringify(value),'; expires=.', exdate.toUTCString(), '; domain=.', window.location.host.toString(), '; path=/;'].join('');
+//	document.cookie = cookie;
+//}
+
+function Save() {
+localStorage.setItem()
+}
 
 function counter() {
-    if (SessionCounter > 0) {
-        SessionCounter = SessionCounter + 1
+    if (SessionCounter >= 0) {
+        SessionCounter = SessionCounter + 1;
         document.getElementById("sessions").innerHTML = SessionCounter;
     }
     else {
-        SessionCounter = 0
+        SessionCounter = 0,
             CountSession();
     }
 }
@@ -252,8 +295,8 @@ function Preset(){
     General
 }
 
-function SwitchCategory(){
-}
+//function SwitchCategory(){
+//}
 
 function HarvestGrain(){
     if (Grain.Total + Synthe.Total < Grain.Max){
